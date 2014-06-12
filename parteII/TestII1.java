569972b1b15363280152caee11b9741a5db40d99
Andrea Peretti <peretti.and@gmail.com>
2014-06-12 23:21:59 +0200

    Edited build.xml: removed redundant call

--- parteII/TestII1.java
+++ parteII/TestII1.java
@@ -3,9 +3,9 @@ import org.antlr.runtime.*;
 public class TestII1 {
 	public static void main ( String [] args ) throws Exception {
 		ANTLRInputStream input = new ANTLRInputStream ( System.in );
-		II1Lexer lexer = new II1Lexer (input );
+		II4Lexer lexer = new II4Lexer (input );
 		CommonTokenStream tokens = new CommonTokenStream ( lexer );
-		II1Parser parser = new II1Parser ( tokens );
+		II4Parser parser = new II4Parser ( tokens );
 		parser.start();
 	}
 }
